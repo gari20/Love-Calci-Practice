3b3eed56c840440f23b4c782f41d2711a368c043
gari20 <74608301+gari20@users.noreply.github.com>
2021-09-16 16:54:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+prompt("Enter your name ");
+prompt("Enter your lover's name ");
+var love=Math.random()*100;
+var loveF=Math.floor(love);
+if(loveF<50)
+{
+    alert("OOPS! :( you have a low score "+loveF+"%");
+}
+else
+{
+alert(" WOHOOOO!! <3 :) Your love score "+loveF+"%");
+}
